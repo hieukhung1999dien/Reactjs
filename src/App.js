21895d83d0b3b2bf4435476d2696e197a6456901
HieuNT103 <hieulinh577@gmail.com>
2021-08-09 09:59:43 -0700

    all

--- src/App.js
+++ src/App.js
@@ -1,40 +1,70 @@
 // App.js
-
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import logo from './img/abc.jpg';
+// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import Create from './components/create.component';
-import Edit from './components/edit.component';
-import Index from './components/index.component';
+// import Create from './components/create.component';
+// import Edit from './components/edit.component';
+// import Index from './components/index.component';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+      }
+
+    handleClick() {
+        document.body.style.background = `url(${logo})`;
+      }
     render() {
         return (
-            <Router>
-                <div className="container">
-                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav mr-auto">
-                                <li className="nav-item">
-                                    <Link to={'/'} className="nav-link">Home</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to={'/create'} className="nav-link">Create</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to={'/index'} className="nav-link">Index</Link>
-                                </li>
-                            </ul>
+            <div>
+                <div class="site-wrapper">
+
+                    <div class="site-wrapper-inner">
+
+                        <div class="cover-container">
+
+                            <div class="masthead clearfix">
+                                <div class="inner">
+                                    {/* <h3 class="masthead-brand">Cover</h3> */}
+                                    <nav>
+                                        <ul class="nav masthead-nav">
+                                            <li class="active"><a href="#">Home</a></li>
+                                            <li><a href="#">Features</a></li>
+                                            <li><a href="#">Contact</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+
+                            <div class="inner cover">
+                                <h1 class="cover-heading">You will marry me?</h1>
+                                <p class="lead">In this life we can no do great things. We can only do small things with great love</p>
+                                <div class="leader">
+                                <p class="leader1">
+                                    <button  class="btn btn-lg btn-default" onClick={this.handleClick}>Yes</button>
+                                </p>
+                                <p class="leader2">
+                                    <button  class="btn btn-lg" >No</button>
+                                </p>
+                                </div>
+                            </div>
+
+                            <div class="mastfoot">
+                                <div class="inner">
+                                    <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+                                </div>
+                            </div>
+
                         </div>
-                    </nav> <br/>
-                    <h2>Create app CRUD with ReactJs, NodeJs, MongoDB</h2> <br/>
-                    <Switch>
-                        <Route exact path='/create' component={ Create } />
-                        <Route path='/edit/:id' component={ Edit } />
-                        <Route path='/index' component={ Index } />
-                    </Switch>
+
+                    </div>
                 </div>
-            </Router>
+                <div id="shadowMeasureIt"></div><div id="divCoordMeasureIt"></div><div id="divRectangleMeasureIt"><div id="divRectangleBGMeasureIt"></div></div>
+            
+            
+            </div>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 // index.js
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './css/cover.css'
+
+
 
 import App from './App';
 
